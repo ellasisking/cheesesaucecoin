e6f0ab8a5453224305ffccb7c2f983ad5459c670
Stratos <iamstratos@gmail.com>
2024-03-15 21:28:07 -0400

    Update Logo Pic To React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Socials from "./components/Socials";
+import { Box, Container, Link, Image } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
-        <a href="https://cheesesaucecoin.vercel.app/" target="_blank">
-          <img
-            src="/cheesesaucelogo.jpg"
-            className="logo"
-            alt="Cheese Sauce logo"
-          />
-        </a>
+        <a href="index.html" target="_blank"></a>
       </div>
-      <h1>Cheesesauce Coin</h1>
+
       <div className="card">
-        <p>Now serving the cheesiest, gooy-est cheese on Plant Radix!</p>
+        <h1>Cheesesauce Coin</h1>
+        <Container maxW="2xl" centerContent>
+          <Box boxSize="sm">
+            <Image src="/cheesesaucelogo.jpg" alt="Cheese Sauce logo" />
+          </Box>
+          <Box padding="4" bg="yellow.200" color="black" maxW="md">
+            Now serving the cheesiest, gooiest cheese on Planet Radix!
+          </Box>
+        </Container>
+        <br />
+        <Link
+          href="https://ociswap.com/resource_rdx1t5d35pz7ej6m6p46zzkzrwegap5qp6cd9qhvsslxw6eyslmzsngn75"
+          size="xl"
+        >
+          Buy on Ociswap!
+        </Link>
         <br />
         <p>
           <Socials />
